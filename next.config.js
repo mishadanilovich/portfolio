34aadae19d9a3c3f9a8450670e1ff43f0c65383e
Mihail Danilovich <danilovich36misha@gmail.com>
2023-02-15 18:59:06 +0100

    Setup eslint, prettier and import aliases

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+	reactStrictMode: true,
+	poweredByHeader: false,
+	optimizeFonts: false,
+	env: {
+		APP_URL: process.env.REACT_APP_URL,
+		APP_ENV: process.env.REACT_APP_ENV
+	}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
